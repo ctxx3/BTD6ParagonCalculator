7241c495d906bfc3dac1b275c947c95a491e820c
ctx3 <woojtekminecraft@gmail.com>
2025-04-22 04:44:18 +0200

    cleaning up the mess left by not specyfing .gitignore

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.BHLT_Cs_.js","_app/immutable/chunks/JX3FnoaB.js","_app/immutable/chunks/B186LnYx.js","_app/immutable/chunks/CqhwPZtg.js","_app/immutable/chunks/DMMo-42K.js","_app/immutable/chunks/B340SDio.js"];
-export const stylesheets = [];
-export const fonts = [];
